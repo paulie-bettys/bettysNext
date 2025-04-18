40e5eb5eef36729e5947a8747763f851747b2230
paulie-bettys <paulrcooper.is@gmail.com>
2025-04-18 14:32:02 +1000

    add placeholder image and text to homepage  Changes to be committed: 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Navbar from "../../components/Navbar";
+import Image from "next/image";
 import './globals.css'
 
 export default function Home() {
@@ -10,9 +11,27 @@ export default function Home() {
         <header className="sticky">
           <Navbar />
         </header>
-          <div className="pt-35">
+          <div className="pt-40">
             <p className="text-center text-green-700 text-2xl">
-              <span className="block">Hello and welcome to Betty Spears!</span>
+              <span className="block">Welcome to Betty Spears Childcare Centre</span>
+            </p>
+          </div>
+          <div className="flex justify-center pt-10">
+            <Image
+              src='/terrace.jpg'
+              alt='Picture of the Betty Spears Terrace on a beautiful day'
+              width="800"
+              height="800"
+              className="cursor-pointer"
+              priority
+          />
+          </div>
+          <div className="flex flex-col gap-3 pt-10">
+            <p className="text-center text-black">
+              <span className="block">Betty Spears Child Care Centre is a well respected, community based, not for profit, Long Day Care Centre operating in Tempe since 1986.</span>
+            </p>
+            <p className="text-center text-black">
+              <span className="block">Our centre provides high quality care and education to children aged 6 weeks to 6 years between the hours of 7.30am and 6.00pm Monday to Friday.</span>
             </p>
           </div>
         </div>
