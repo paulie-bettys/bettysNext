4a2be3f98fa76932209315def1e3e3f73799e145
paulie-bettys <paulrcooper.is@gmail.com>
2025-04-19 09:16:54 +1000

    adding placeholder text and images to other pages

--- src/app/bettyslife/page.tsx
+++ src/app/bettyslife/page.tsx
@@ -1,8 +1,38 @@
 'use client'
 
+import Image from 'next/image';
 import Navbar from '../../../components/Navbar';
 import '../globals.css'
 
 export default function Home() {
-    return (<h1>Life at Betty's</h1>);
+    return (
+      <div className="bg-white">
+        <header className="sticky">
+          <Navbar />
+        </header>
+          <div className="pt-40">
+            <p className="text-center text-green-700 text-2xl">
+              <span className="block">Life at Betty's</span>
+            </p>
+          </div>
+          <div className="flex justify-center pt-10">
+            <Image
+              src='/garden-path.jpg'
+              alt='A picture of the garden path to the Bettys bush garden'
+              width="800"
+              height="800"
+              className="cursor-pointer"
+              priority
+          />
+          </div>
+          <div className="flex flex-col gap-3 pt-10">
+            <p className="text-center text-black">
+              <span className="block">Stuff about daily routine at Betty's will go here. Maybe mealtimes, schedules, excursions, and so on.</span>
+            </p>
+            <p className="text-center text-black">
+              <span className="block">Maybe also something about learning frameworks, different rooms, curriculum, that sort of thing?</span>
+            </p>
+          </div>
+        </div>
+    );
   }
--- src/app/whybettys/page.tsx
+++ src/app/whybettys/page.tsx
@@ -1,12 +1,38 @@
 'use client'
 
 import Navbar from '../../../components/Navbar';
+import Image from 'next/image';
 import '../globals.css'
 
 export default function Home() {
     return (
-      <>
-        <Navbar />
-      </>
+      <div className="bg-white">
+        <header className="sticky">
+          <Navbar />
+        </header>
+          <div className="pt-40">
+            <p className="text-center text-green-700 text-2xl">
+              <span className="block">Why Bettys?</span>
+            </p>
+          </div>
+          <div className="flex justify-center pt-10">
+            <Image
+              src='/bettys-from-terrace.jpg'
+              alt='Picture of Betty spears rooms from the terrace'
+              width="800"
+              height="800"
+              className="cursor-pointer"
+              priority
+          />
+          </div>
+          <div className="flex flex-col gap-3 pt-10">
+            <p className="text-center text-black">
+              <span className="block">This page could make the case for why your kid should attend Betty's.</span>
+            </p>
+            <p className="text-center text-black">
+              <span className="block">Lots of stuff we could possibly put here, although maybe it should just go on the home page?</span>
+            </p>
+          </div>
+        </div>
     );
   }
