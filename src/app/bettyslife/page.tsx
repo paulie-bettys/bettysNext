836ea8f3543bae9b6a67e11a4b75a9184c9401df
paulie-bettys <paulrcooper.is@gmail.com>
2025-04-23 16:00:12 +1000

    update navbar and add waitlist form code

--- src/app/bettyslife/page.tsx
+++ src/app/bettyslife/page.tsx
@@ -2,12 +2,12 @@
 import Image from 'next/image';
 import '../globals.css'
 
-export default function Home() {
+export default function Life() {
     return (
       <div className="bg-white">
           <div className="pt-40">
             <p className="text-center text-green-700 text-2xl">
-              <span className="block">Life at Betty's</span>
+              <span className="block">Life at Betty&apos;s</span>
             </p>
           </div>
           <div className="flex justify-center pt-10">
@@ -22,7 +22,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col gap-3 pt-10">
             <p className="text-center text-black">
-              <span className="block">Stuff about daily routine at Betty's will go here. Maybe mealtimes, schedules, excursions, and so on.</span>
+              <span className="block">Stuff about daily routine at Betty&apos;s will go here. Maybe mealtimes, schedules, excursions, and so on.</span>
             </p>
             <p className="text-center text-black">
               <span className="block">Maybe also something about learning frameworks, different rooms, curriculum, that sort of thing?</span>
---
+++ src/app/waitlist/page.tsx
@@ -0,0 +1,10 @@
+import WaitlistForm from "../../../components/WaitlistForm";
+import "../globals.css"
+
+export default function Waitlist(){
+    return(
+        <div className="flex flex-col items-center justify-center p-40 bg-white">
+            <WaitlistForm />
+        </div>
+    )
+}
